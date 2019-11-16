be9c7f8c7a7497108a3d552c771c0298652b62dc
YUICHIRO WADA <wada1257@gmail.com>
2019-11-16 23:13:13 +0900

    userページの作成＆確認用のルートの記載

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
+Route::get('/', function () {
+    return view('book.index');
+});
+Route::get('/index', function () {
+    return view('book.index');
+});
+Route::get('/show', function () {
+    return view('book.show');
+});
+Route::get('/mypage', function () {
+    return view('book.mypage');
+});
+Route::get('/user', function () {
+    return view('book.user');
+});
+
 Route::get('review', 'BookReviewController@index')->name('book.index');
 Route::get('review/show/{isbn}', 'BookReviewController@bookReviewList')->name('book.show');
 Route::get('review/{user_id}/mypage', 'BookReviewController@myBookReviewHistory')->name('book.mypage');

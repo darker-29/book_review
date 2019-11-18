112fb9b7898e75def9dd73b6bb69b87f8ec39684
YUICHIRO WADA <wada1257@gmail.com>
2019-11-18 14:26:01 +0900

    評価の星の変化追加＆ユーザー一覧のバグの改修

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -69,6 +69,18 @@
                     </a>
                     <p class="modal-down-avatar-name">華子<p>
                 </div>
+                <div class="modal-down-user">
+                    <a class="" href="">
+                        <img class="modal-down-avatar" src="/image/konanLogo.png" alt="ユーザーアバター">
+                    </a>
+                    <p class="modal-down-avatar-name">先生<p>
+                </div>
+                <div class="modal-down-user">
+                    <a class="" href="">
+                        <img class="modal-down-avatar" src="/image/MaedaLogo.png" alt="ユーザーアバター">
+                    </a>
+                    <p class="modal-down-avatar-name">前田<p>
+                </div>
             </div>
         </div>
     </div>
